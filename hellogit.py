e7358a533a432b17a6f49b3b93c2280a6d20ecec
L-DevOps-01 <luisdevops01@gmail.com>
2024-12-09 21:40:22 +0100

    Fin de Pruebas pora ahora en VMWare

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,3 @@
 print("Este es mi primer archivo. Ahora es nuevo en la nube")
 print ("Esto es un cambio desde Ubuntu VMW")
-## Intentaré hacer un push a la máquina virtual
+## Intentaré hacer un pull a la máquina virtual
